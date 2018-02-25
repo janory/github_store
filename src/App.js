f2a81414acd1193427301933be49039053648c60
Janos Szathmary <szathmary.janos89@gmail.com>
2018-02-25 18:49:09 +0100

    formatted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
-import Grid from 'material-ui/Grid';
+import Grid from "material-ui/Grid";
 
 export default class App extends Component {
   render() {
-    return (<div>
-      <Header/>
-      <Grid container spacing={0}>
-        <Grid item xs={2}>
+    return (
+      <div>
+        <Header />
+        <Grid container spacing={0}>
+          <Grid item xs={2} />
+          <Grid item xs={8}>
+            {this.props.children}
+          </Grid>
+          <Grid item xs={2} />
         </Grid>
-        <Grid item xs={8}>
-          {this.props.children}
-        </Grid>
-        <Grid item xs={2}>
-        </Grid>
-      </Grid>
       </div>
     );
   }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
-import AppBar from 'material-ui/AppBar';
+import AppBar from "material-ui/AppBar";
 
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
 
 export default class Header extends Component {
   render() {
-    return      (       <AppBar position="static" >
-      <Toolbar>
-        <Typography variant="title" color="inherit">
-          GitHub Store
-        </Typography>
-      </Toolbar>
-    </AppBar>);
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            GitHub Store
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
