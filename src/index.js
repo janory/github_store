63c516c31e06d2026ab2cbe260b4076d2395f346
Janos Szathmary <szathmary.janos89@gmail.com>
2018-02-25 17:28:07 +0100

    material ui added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import routes from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import App from "./App";
 
 const app = document.getElementById("root");
 
 ReactDOM.render(
-  <Router path="/" component={App}>
-    {routes}
-  </Router>,
+  <MuiThemeProvider>
+    <Router path="/" component={App}>
+      {routes}
+    </Router>
+  </MuiThemeProvider>,
   app
 );
--- src/views/UserSearchView.js
+++ src/views/UserSearchView.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import RaisedButton from "material-ui/RaisedButton";
 
 export default class UserSearchView extends Component {
   render() {
-    return <h1>User search</h1>;
+    return (
+      <div>
+        <h1>User search</h1>
+        <RaisedButton label="Default" />
+      </div>
+    );
   }
 }
