d70e74932ae8e82f87649aa33dced7d6594532bb
Janos Szathmary <szathmary.janos89@gmail.com>
2018-02-25 21:44:26 +0100

    first API calls

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,8 +1,47 @@
 import * as types from "../constants/ActionTypes";
+import config from "../config";
 
-export const searchUser = username => ({
-  type: types.FETCH_USER_FINISHED,
-  payload: {
-    username
+
+export const searchUser = username => async (dispatch) => {
+  dispatch({
+    type: types.FETCH_USER_STARTED,
+    payload: {
+      username
+    }
+  });
+
+  try {
+    const response = await fetch(
+      `${config.githubApi}/users/${username}/repos`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json"
+        }
+      }
+    );
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const data = await response.json();
+
+    dispatch({
+      type: types.FETCH_USER_FINISHED,
+      payload: {
+        data
+      }
+    });
+
+    return true;
+  } catch (e) {
+    dispatch({
+      type: types.FETCH_USER_FAILED,
+      payload: {
+        error: e.message
+      }
+    });
+    return false;
   }
-});
+};
